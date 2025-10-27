409bbe8ffcc59e3216611b2c93b5a38e4183b5b2
luke-d-moore <luke.d.moore1@gmail.com>
2025-10-27 17:31:29 +0000

    change to allow my front end apps to send requests to the api
    change to allow my front end apps to send requests to the api

--- AutoTradeSystem/AutoTradeSystem/Program.cs
+++ AutoTradeSystem/AutoTradeSystem/Program.cs
@@ -5,7 +5,16 @@ using AutoTradeSystem.Logging;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        policy =>
+        {
+            policy.AllowAnyOrigin()
+                  .AllowAnyMethod()
+                  .AllowAnyHeader();
+        });
+});
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -26,7 +35,6 @@ builder.Services.AddHostedService(p => p.GetRequiredService<IPricingService>());
 //builder.Services.AddSingleton<IHighFrequencyAutoTradingService, HighFrequencyAutoTradingService>();
 //builder.Services.AddHostedService(p => p.GetRequiredService<IHighFrequencyAutoTradingService>());
 
-
 var app = builder.Build();
 
 app.UseDefaultFiles();
@@ -38,6 +46,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors();
 
 app.UseHttpsRedirection();
 
